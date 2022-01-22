7da423e62f8f494f9c5161e6f36a81875d2a4da3
nelumadan <nelu_madan@yahoo.com>
2022-01-21 21:14:58 -0800

    This is my second commit

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export PATH="$PATH:/Users/Neelam/apache-maven-3.8.4/bin"
-echo $PATH
 cd /Users/Neelam/eclipse-workspace/myfirstmavenproject
 mvn clean install
--- src/test/java/com/Julybatch/AppTest.java
+++ src/test/java/com/Julybatch/AppTest.java
@@ -1,7 +1,5 @@
 package com.Julybatch;
 
-import org.testng.annotations.Test;
-import org.testng.AssertJUnit;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,13 +9,13 @@ public class AppTest
 	public void testLogin1()
 	{
 		App myapp=new App();//creating an object of the App class.
-		AssertJUnit.assertEquals(0,myapp.userLogin("abc", "abc123"));//comparing the result from App class by calling the method from that class and providing parameters
+		Assert.assertEquals(0,myapp.userLogin("abc", "abc123"));//comparing the result from App class by calling the method from that class and providing parameters
 	}
 	
 	@Test
 	public void testLogin2()
 	{
 		App myapp=new App();
-		AssertJUnit.assertEquals(1,myapp.userLogin("abc", "abc@123"));
+		Assert.assertEquals(1,myapp.userLogin("abc", "abc@123"));
 	}
 }
